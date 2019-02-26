5c81f8b03db7d772825722caa625d2ec595a0f4c
Sorin Banica <sbanica2@gmail.com>
2019-02-26 19:28:23 +0200

    minor

--- src/app/keyvalue-unsorted.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { KeyvalueUnsortedPipe } from './keyvalue-unsorted.pipe';
-
-describe('KeyvalueUnsortedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new KeyvalueUnsortedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
